33cf70df843c7c53b640a3c4b1b4a0bb2539063d
Imelda Hope <imeldalarusso@gmail.com>
2024-08-08 05:07:16 +0300

    uses nested routes

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
 function UserCard({user}) {
+    //Demonstrating accessing outlet context within deeply nested children.
+    //NB: not using this users anywhere except just logging it.
+    const users = useOutletContext();
+    console.log(users);
+
     return (
       <article>
           <h2>{user.name}</h2>
